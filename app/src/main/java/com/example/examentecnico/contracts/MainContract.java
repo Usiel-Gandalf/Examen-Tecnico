86f0d8b6e216b433fd6f7e0880bfcf6180d74392
Usiel <usieloficial@gmail.com>
2021-06-29 21:00:37 -0500

    logout

--- app/src/main/java/com/example/examentecnico/contracts/MainContract.java
+++ app/src/main/java/com/example/examentecnico/contracts/MainContract.java
@@ -1,9 +1,14 @@
 package com.example.examentecnico.contracts;
 
+import android.view.Menu;
+import android.view.MenuItem;
+
 public interface MainContract {
 
     interface View{
+        boolean onCreateOptionsMenu(Menu menu);
 
+        boolean onOptionsItemSelected(MenuItem item);
     }
 
     interface Presenter{
--- app/src/main/java/com/example/examentecnico/views/MainView.java
+++ app/src/main/java/com/example/examentecnico/views/MainView.java
@@ -2,10 +2,16 @@ package com.example.examentecnico.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.examentecnico.R;
 import com.example.examentecnico.contracts.MainContract;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainView extends AppCompatActivity implements MainContract.View {
 MainContract.Presenter presenter;
@@ -14,5 +20,25 @@ MainContract.Presenter presenter;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.singout:
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(this, "Sesion cerrada", Toast.LENGTH_LONG).show();
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
