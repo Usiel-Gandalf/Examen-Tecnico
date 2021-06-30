4389473b2c339e6ae90d2bef96fd992d8e52e8d3
Usiel <usieloficial@gmail.com>
2021-06-29 20:54:06 -0500

    Login

--- app/src/main/java/com/example/examentecnico/contracts/LoginContract.java
+++ app/src/main/java/com/example/examentecnico/contracts/LoginContract.java
@@ -1,16 +1,50 @@
 package com.example.examentecnico.contracts;
+import android.content.Context;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public interface LoginContract {
 
-    interface view{
+    interface View{
+        void initElements();
 
+        void showMessage(String Message);
+
+        void showErrorPassword(String message);
+
+        void showErrorUser(String message);
+
+        void showProgressBar();
+
+        void hideProgressBar();
+
+        void sessionSuccess();
     }
 
     interface Presenter{
+        void showMessage(String message);
 
+        boolean validatePasswordLenght(String password);
+
+        boolean validateAplhaPassword(String password);
+
+        boolean validateTypeUserEmail(String email);
+
+        boolean isUserEmpty(String email);
+
+        boolean isPasswordEmpty(String password);
+
+        void sesionSuccess();
+
+        void sesion(String usuario, String password, FirebaseAuth auth, Context context);
+
+        void createUser(String usuario, String password, FirebaseAuth auth, Context context);
     }
 
-    interface model{
+    interface Model{
 
+        void sesionFBase(String usuario, String password, LoginContract.Presenter listener, FirebaseAuth auth, Context context);
+
+        void createFBaseUser(String usuario, String password, LoginContract.Presenter listener, FirebaseAuth auth, Context context);
     }
 }
--- app/src/main/java/com/example/examentecnico/models/LoginModel.java
+++ app/src/main/java/com/example/examentecnico/models/LoginModel.java
@@ -1,7 +1,73 @@
 package com.example.examentecnico.models;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
 import com.example.examentecnico.contracts.LoginContract;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
-public class LoginModel implements LoginContract {
+public class LoginModel implements LoginContract.Model {
+    private Context context;
+    private FirebaseAuth auth;
 
+    @Override
+    public void sesionFBase(String usuario, String password, LoginContract.Presenter listener, FirebaseAuth auth, Context context) {
+        auth.signInWithEmailAndPassword(usuario, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //listener.showMessage("Sesion Exitosa");
+                            listener.sesionSuccess();
+                        } else {
+                            String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                            switch (errorCode){
+                                case "ERROR_USER_NOT_FOUND":
+                                    listener.showMessage("Usuario inexistente");
+                                    break;
+
+                                case "ERROR_WRONG_PASSWORD":
+                                    listener.showMessage("Contraseña invalida");
+                                    break;
+
+                                case "ERROR_INVALID_EMAIL":
+                                    listener.showMessage("Tipo de Email incorrecto");
+                                    break;
+                            }
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void createFBaseUser(String usuario, String password, LoginContract.Presenter listener, FirebaseAuth auth, Context context) {
+        auth.createUserWithEmailAndPassword(usuario, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            listener.showMessage("Registro de usuario Exitoso");
+                            listener.sesionSuccess();
+                        } else {
+                            String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                            switch (errorCode){
+                                case "ERROR_EMAIL_ALREADY_IN_USE":
+                                    listener.showMessage("Usuario registrado, inicie sesion");
+                                    break;
+
+                                case "ERROR_INVALID_EMAIL":
+                                    listener.showMessage("Tipo de Email incorrecto");
+                                    break;
+                            }
+                        }
+                    }
+                });
+    }
 }
